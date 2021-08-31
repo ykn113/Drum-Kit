2e1be6087f87f56c4430f745e67393da05b8eb68
ykn113 <yukinosnj13@gmail.com>
2021-08-31 17:35:22 +0900

    [Update]

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var numberofdrums = document.querySelectorAll(".drum").length;
 for (var i = 0; i < numberofdrums; i++){
 
+
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
     var buttonInnerHTML = this.innerHTML;
     makesound(buttonInnerHTML);
